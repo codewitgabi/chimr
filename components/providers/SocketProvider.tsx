8da8b0a8aaf96b64e33df0bd6a08ade4b15acb65
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2025-04-06 22:11:46 +0100

    feat: add event for updating contact

--- components/providers/SocketProvider.tsx
+++ components/providers/SocketProvider.tsx
@@ -13,6 +13,7 @@ function SocketProvider({ children }: { children: ReactNode }) {
   const setContacts = useAppStore((state) => state.setContacts);
   const setSelectedContact = useAppStore((state) => state.setSelectContact);
   const setChatHistory = useAppStore((state) => state.setChatHistory);
+  const contacts = useAppStore((state) => state.contacts);
 
   useEffect(() => {
     function onConnect() {
@@ -56,18 +57,48 @@ function SocketProvider({ children }: { children: ReactNode }) {
       setChatHistory(chatHistory);
     }
 
+    function onUpdateContact({
+      contactId,
+      timestamp,
+      unreadCount,
+      lastMessage,
+    }: Pick<
+      IChatContact,
+      "contactId" | "timestamp" | "unreadCount" | "lastMessage"
+    >) {
+      // Get contact with contact id and update necessary data
+
+      const newContacts = contacts.map((contact) => {
+        if (contact.contactId === contactId) {
+          return { ...contact, timestamp, unreadCount, lastMessage };
+        }
+
+        return contact;
+      });
+
+      setContacts(newContacts);
+    }
+
     socket.on("connect", onConnect);
     socket.on("disconnect", onDisconnect);
     socket.on("fetch_contacts", onFetchContacts);
     socket.on("chat_history", onGetChatHistory);
+    socket.on("update_contact", onUpdateContact);
 
     return () => {
       socket.off("connect", onConnect);
       socket.off("disconnect", onDisconnect);
       socket.off("fetch_contacts", onFetchContacts);
       socket.off("chat_history", onGetChatHistory);
+      socket.off("update_contact", onUpdateContact);
     };
-  }, [setIsSocketConnected, setContacts, setSelectedContact, setChatHistory]);
+  }, [
+    setIsSocketConnected,
+    setContacts,
+    setSelectedContact,
+    setChatHistory,
+    contacts,
+  ]);
 
   return <>{children}</>;
 }
