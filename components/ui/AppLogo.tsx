e31e9499981faefba3abbaeb0e4e9c45f94a4964
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2025-04-08 12:47:08 +0100

    fix: fix app logo hydration error

--- components/ui/AppLogo.tsx
+++ components/ui/AppLogo.tsx
@@ -4,24 +4,28 @@ import Image from "next/image";
 import LogoLight from "@/assets/app-logo-light.svg";
 import LogoDark from "@/assets/app-logo-dark.png";
 import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
 
 function AppLogo() {
-  const { theme } = useTheme();
+  const { resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <div style={{ width: 129, height: 40 }} />;
+  }
 
   return (
-    <>
-      {theme === "dark" ? (
-        <Image priority src={LogoDark} alt="app-logo" width={129} height={40} />
-      ) : (
-        <Image
-          priority
-          src={LogoLight}
-          alt="app-logo"
-          width={129}
-          height={40}
-        />
-      )}
-    </>
+    <Image
+      src={resolvedTheme === "dark" ? LogoDark : LogoLight}
+      alt="app-logo"
+      width={129}
+      height={40}
+      priority
+    />
   );
 }
 
