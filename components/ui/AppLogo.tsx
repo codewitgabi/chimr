de4188cccae77316b89a0de84e814bcc9e7ab9b3
Gabriel Michael Ojomakpene <gabrielmicheal497@gmail.com>
2025-04-11 16:19:57 +0100

    feat: update logo

--- components/ui/AppLogo.tsx
+++ components/ui/AppLogo.tsx
@@ -1,31 +1,18 @@
-"use client";
-
 import Image from "next/image";
-import LogoLight from "@/assets/app-logo-light.svg";
-import LogoDark from "@/assets/app-logo-dark.png";
-import { useTheme } from "next-themes";
-import { useState, useEffect } from "react";
 
 function AppLogo() {
-  const { resolvedTheme } = useTheme();
-  const [mounted, setMounted] = useState<boolean>(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) {
-    return <div style={{ width: 129, height: 40 }} />;
-  }
-
   return (
-    <Image
-      src={resolvedTheme === "dark" ? LogoDark : LogoLight}
-      alt="app-logo"
-      width={129}
-      height={40}
-      priority
-    />
+    <div className="flex items-center gap-2">
+      <Image
+        src={"/apple-touch-icon.png"}
+        alt="app-logo"
+        width={40}
+        height={40}
+        priority
+      />
+
+      <span className="text-[#209CEE] font-bold">Let&apos;s chat</span>
+    </div>
   );
 }
 
